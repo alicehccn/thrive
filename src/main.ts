f83cae685734b168e64c7e9f3b1c55a4f72c03cb
Alice Huang <alicehccn@gmail.com>
2022-05-28 18:19:32 -0700

    boostrap sessions api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './controller/v1/app.module';
+import { SessionsModule } from './sessions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(SessionsModule);
   await app.listen(3000);
 }
 bootstrap();
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/controller/v1/app.module';
+import { SessionsModule } from '../src/sessions/';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [SessionsModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -17,8 +17,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/sessions')
       .expect(200)
-      .expect('Hello World!');
+      .expect('This action returns all sessions');
   });
 });
