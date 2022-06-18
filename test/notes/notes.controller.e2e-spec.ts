25e3b383fce934ad2091df4e3d76384f3e043376
Alice Huang <alicehccn@gmail.com>
2022-06-17 18:07:18 -0700

    add e2e test and fix current lint

---
+++ test/notes/notes.controller.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "../../src/app.module";
+import { Notes } from "../../src/notes/entities";
+
+describe("AppController (e2e)", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it("GET all notes", () => {
+    return request(app.getHttpServer())
+      .get("/notes")
+      .expect(200)
+      .expect({ notes: Notes });
+  });
+});
