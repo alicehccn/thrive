9a1142dd5a5a4c6e5668669f4cbb5e4c88f8c9f3
Alice Huang <alicehccn@gmail.com>
2022-07-04 01:44:25 -0700

    delete old migrations

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,6 +1,3 @@
-import { CreateTable1656918134563 } from "src/migrations/1656918134563-CreateTable";
-import { CreateTable1656921454113 } from "src/migrations/1656921454113-CreateTable";
-import { CreateAuthor1656922263042 } from "src/migrations/1656922263042-CreateAuthor";
 import { DataSource } from "typeorm";
 
 export default new DataSource({
@@ -12,8 +9,5 @@ export default new DataSource({
   database: 'postgres',
   entities: [],
   migrations: [
-    CreateTable1656918134563,
-    CreateTable1656921454113,
-    CreateAuthor1656922263042
   ],
 });
