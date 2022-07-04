a0f4465643bde69268c9569215010ef2c9491e7e
Alice Huang <alicehccn@gmail.com>
2022-07-04 00:58:08 -0700

    fixing note migration

---
+++ src/migrations/1656921366179-CreateTable.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateTable1656921366179 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS note(
+                id UUID PRIMARY KEY,
+                date VARCHAR (50),
+                title VARCHAR (100),
+                subtitles VARCHAR(100),
+                body text,
+                recommendations integer,
+                author uuid);
+            `);
+      }
+    
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    
+    await queryRunner.query(`
+        DROP TABLE note
+    `);
+    }
+
+}
