550414f2c86668ff3c7f50a216e7a8e9aa77f4f0
Alice Huang <alicehccn@gmail.com>
2022-07-04 01:29:54 -0700

    lint

--- src/migrations/1656922263042-CreateAuthor.ts
+++ src/migrations/1656922263042-CreateAuthor.ts
@@ -1,16 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from "typeorm";
 import { uuid } from "uuidv4";
 
 export class CreateAuthor1656922263042 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             INSERT INTO author(id, name)
             VALUES('${uuid()}', 'admin')
         `);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  }
+  /* eslint-disable @typescript-eslint/no-empty-function */
+  public async down(): Promise<void> {}
 }
