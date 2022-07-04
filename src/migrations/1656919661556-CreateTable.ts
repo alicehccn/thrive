d18e4cc5889a3cc1cd49b9f8bfaa2b427e5f101f
Alice Huang <alicehccn@gmail.com>
2022-07-04 00:47:13 -0700

    create if not exists;

--- src/migrations/1656919661556-CreateTable.ts
+++ src/migrations/1656919661556-CreateTable.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateTable1656919661556 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-    CREATE TABLE note (
+    CREATE TABLE IF NOT EXISTS note(
         id UUID PRIMARY KEY,
         date VARCHAR (50),
         title VARCHAR (100),
