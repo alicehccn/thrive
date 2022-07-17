808ff62e06c44d582964335072fc8bb801c95b88
Alice Huang <alicehccn@gmail.com>
2022-07-17 16:21:54 -0700

    run migration for ec2 remotely

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,21 +1,14 @@
 import { CreateAuthor1658015205318 } from "src/migrations/1658015205318-create-author";
-import { AuthorTimestamp1658022331591 } from "src/migrations/1658022331591-author-timestamp";
 import { Author, Note } from "src/notes/entities";
 import { DataSource } from "typeorm";
 
 export default new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'alicehuang',
-  password: 'postgres',
-  database: 'postgres',
-  entities: [
-    Note,
-    Author
-  ],
-  migrations: [
-    CreateAuthor1658015205318,
-    AuthorTimestamp1658022331591,
-  ]
+  type: "postgres",
+  host: "ec2-44-242-15-100.us-west-2.compute.amazonaws.com",
+  port: 35000,
+  username: "postgres",
+  password: "postgres",
+  database: "db",
+  entities: [Note, Author],
+  migrations: [CreateAuthor1658015205318],
 });
