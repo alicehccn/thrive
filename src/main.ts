9989600060944912602e7503ac07abaa9f446965
Alice Huang <alicehccn@gmail.com>
2022-07-24 18:50:18 -0700

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import cors from 'cors';
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +14,6 @@ async function bootstrap(): Promise<void> {
       },
     })
   );
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
